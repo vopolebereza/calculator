961af326ea8f9438807eac529be62a657f6b4ab8
Iuliia Burmistrova <vopolebereza@yandex.ru>
2025-12-20 16:47:11 +0400

    Добавлена поддержка возведения в степень (**)

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ print("Доступные операции: + - * /")
 # Получаем числа и операцию от пользователя
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
-operation = input("Введите операцию (+, -, *, /): ")
+operation = input("Введите операцию (+, -, *, /, **): ")
 
 # Выполняем расчёт
 if operation == "+":
@@ -24,5 +24,10 @@ elif operation == "/":
         print(f"{num1} / {num2} = {result}")
     else:
         print("Ошибка: деление на ноль!")
+elif operation == "**":
+    result = 1
+    for _ in range(num2):
+        result *= num1
+    print(f"{num1} ** {num2} = {result}")
 else:
     print("Ошибка: неизвестная операция")
